5b2639cbf742550b0751bd0136eb1687df7d47cb
adas <ajanta744@gmail.com>
2022-03-15 23:17:20 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// selector
+
+const search = document.querySelector('.search');
+const submit = document.querySelector('.submit');
+var city = document.querySelector('.city');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+
+// Generate API key from openweatehrmap.org and add it after "&appid= "
+
+submit.addEventListener("click", function () {
+    fetch( 
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          search.value +
+          "&appid= add your API key in here"
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+         
+
+          var cityVal = data.name;
+          var tempVal = data['main']['temp'];
+          var desVal = data['weather'][0]['description'];
+          city.innerHTML = `City : ${cityVal}`;
+          temp.innerHTML =`Temparature : ${Math.floor(tempVal -273)} degree`;
+          desc.innerHTML = `Cloud: ${desVal}` ;
+          search.value="";
+      } 
+    )
+    .catch((err)=> {err.message});
+        
+} );
+
+
